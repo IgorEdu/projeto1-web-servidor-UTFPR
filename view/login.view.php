8956bc4fcbb2b88171f04c85ff46c74eb7adaece
Igor <igoreduardo20101@gmail.com>
2024-10-23 16:16:14 -0300

    feat: estrutura HTML do formulário

--- view/login.view.php
+++ view/login.view.php
@@ -6,10 +6,30 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="" method="post">
-        <input type="text" name="username" id="username">
-        <input type="password" name="password" id="password">
-        <input type="submit" value="Login">
-    </form>
+    <div class="form-container">
+        <form action="" method="post" id="login-form">
+            <div class="form-title">
+                <h2>Login</h2>
+            </div>
+            <div id="msg-autenticacao" hidden>
+                <p>Usuário não autenticado!</p>
+            </div>
+            <div class="container-label">
+                <label for="username">Usuário: </label>
+                <input type="text" name="username" id="username" required>
+            </div>
+            <div class="container-label">
+                <label for="password">Senha: </label>
+                <input type="password" name="password" id="password" required>
+            </div>
+            <div>
+                <button type="button" id="login-button">Entrar</button>
+            </div>
+            <div>
+                <span>Não possui uma conta?</span>
+                <a href="#">Criar</a>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
