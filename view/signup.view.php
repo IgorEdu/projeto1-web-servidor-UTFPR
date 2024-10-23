525e6d1b0f3e11f8e0ae1cf69a4419ffea73ca44
Igor <igoreduardo20101@gmail.com>
2024-10-22 21:12:48 -0300

    feat: adiciona página de login e de cadastro de usuário

---
+++ view/signup.view.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>SignUp</h1>
+    <form action="" method="post">
+        <input type="text" name="username" id="username">
+        <input type="email" name="email" id="email">
+        <input type="password" name="password" id="password">
+        <input type="password" name="confirm-password" id="confirm-password">
+        <input type="submit" value="Cadastrar">
+    </form>
+</body>
+</html>
