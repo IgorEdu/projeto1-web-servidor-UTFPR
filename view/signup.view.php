f36e9de2dc9699c3923993e4fe2fe7816444a08b
Igor <igoreduardo20101@gmail.com>
2024-10-24 21:16:16 -0300

    feat: estrutura HTML do formulario de cadastro de usuário

--- view/signup.view.php
+++ view/signup.view.php
@@ -6,13 +6,34 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>SignUp</h1>
-    <form action="" method="post">
-        <input type="text" name="username" id="username">
-        <input type="email" name="email" id="email">
-        <input type="password" name="password" id="password">
-        <input type="password" name="confirm-password" id="confirm-password">
-        <input type="submit" value="Cadastrar">
-    </form>
+    <div class="form-container">
+        <form action="" method="post" id="login-form">
+            <div class="form-title">
+                <h2>Cadastro</h2>
+            </div>
+            <div id="msg-cadastro" hidden>
+                <p>Usuário já cadastrado!</p>
+            </div>
+            <div class="container-label">
+                <label for="username">Usuário: </label>
+                <input type="text" name="username" id="username" required>
+            </div>
+            <div class="container-label">
+                <label for="password">Senha: </label>
+                <input type="password" name="password" id="password" required>
+            </div>
+            <div class="container-label">
+                <label for="password-confirm">Confirme a senha: </label>
+                <input type="password" name="password-confirm" id="password-confirm" required>
+            </div>
+            <div>
+                <button type="button" id="signup-button">Cadastrar</button>
+            </div>
+            <div>
+                <span>Já possui uma conta?</span>
+                <a href="#">Criar</a>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
