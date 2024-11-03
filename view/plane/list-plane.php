bbab8c986225aee1a716988d4150a086471f7f83
Igor <igoreduardo20101@gmail.com>
2024-11-03 09:48:28 -0300

    fix: corrige página de listagem de aviões

--- view/plane/list-plane.php
+++ view/plane/list-plane.php
@@ -1,25 +1,9 @@
-<?php 
-require_once '../../entities/Plane.php';
-require_once '../../infra/ConnectionDB.php';
-// $plane1 = new Plane(1, 'a90192', 'airbus', '2018');
-// $plane2 = new Plane(2, 'a90289', 'airbus', '2019');
-$db = ConnectionDB::getInstance();
-$query = $db->prepare("SELECT * FROM planes");
-$query->execute();
-$dbplanes = $query->fetchAll(PDO::FETCH_OBJ);
-$planes = [];
-foreach ($dbplanes as $dbplan) {
-    $planes[] = new Plane($dbplan->code, $dbplan->model, $dbplan->total_seats, $dbplan->id );
-}
-// $planes = [$plane1, $plane2];
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Lista de Aviões</title>
 </head>
 <body>
     <h1>Aviões</h1>
@@ -29,14 +13,20 @@ foreach ($dbplanes as $dbplan) {
             <th>Modelo</th>
             <th>Total de assentos</th>
         </tr>
-        <?php foreach ($planes as $plane): ?>
+        <?php if (!empty($planes) && is_array($planes)): ?>
+            <?php foreach ($planes as $plane): ?>
+                <tr>
+                    <td><?= $plane->getCode() ?></td>
+                    <td><?= $plane->getModel() ?></td>
+                    <td><?= $plane->getTotalSeats() ?></td>
+                </tr>
+            <?php endforeach; ?>
+        <?php else: ?>
             <tr>
-                <td><?= $plane->getCode() ?></td>
-                <td><?= $plane->getModel() ?></td>
-                <td><?= $plane->getTotalSeats() ?></td>
+                <td colspan="3">Nenhum avião encontrado.</td>
             </tr>
-        <?php endforeach; ?>
-        <a href="#">Criar</a>
+        <?php endif; ?>
     </table>
+    <a href="edit-plane.php">Criar</a>
 </body>
 </html>
