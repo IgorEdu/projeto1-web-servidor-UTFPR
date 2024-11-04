f2abc41b56ba9ad79984474964ba94e64792c4b6
Igor <igoreduardo20101@gmail.com>
2024-11-04 14:22:01 -0300

    feat: adiciona controle de rotas

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php 
-// require("view/home.php");
-require("controller/checkAuthentication.php");
+    $rota = explode('/', substr($_SERVER['REQUEST_URI'], 1));
+
+    $recurso = empty($rota[0]) ? 'home' : $rota[0];
+
+    $controlador = "controller/$recurso.controller.php";
+    
+    $acao = empty($rota[1]) ? "list" : $rota[1];
+
+    if (file_exists($controlador)) {
+        require($controlador);
+    } else {
+        require("controllers/404.controller.php");
+    }
